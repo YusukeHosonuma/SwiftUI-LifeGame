9f9fddddf4b360010256264b8319f1656ae9e17d
Yusuke Hosonuma <tobi462@gmail.com>
2020-07-15 13:29:52 +0900

    Initial Commit

---
+++ Shared/LifeGameAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeGameAppApp.swift
+//  Shared
+//
+//  Created by Yusuke Hosonuma on 2020/07/15.
+//
+
+import SwiftUI
+
+@main
+struct LifeGameAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
