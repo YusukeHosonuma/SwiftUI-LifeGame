edca72d44d7337b1bafc5879230f5ca3d6e92649
Yusuke Hosonuma <tobi462@gmail.com>
2020-07-25 19:26:55 +0900

    improve: edit color in preference when on mac

--- Shared/App/LifeGameApp.swift
+++ Shared/App/LifeGameApp.swift
@@ -7,12 +7,20 @@
 
 import SwiftUI
 
+private let settingEnvironment = SettingEnvironment()
+
 @main
 struct LifeGameApplication: App {
     var body: some Scene {
         WindowGroup {
             ContainerView()
-                .environmentObject(SettingEnvironment())
+                .environmentObject(settingEnvironment)
         }
+        #if os(macOS)
+        Settings {
+            PreferenceView()
+                .environmentObject(settingEnvironment)
+        }
+        #endif
     }
 }
