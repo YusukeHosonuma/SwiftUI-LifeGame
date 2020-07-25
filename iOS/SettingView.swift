a206749c11ef1d39bddf5efb3157c664af45fe7b
Yusuke Hosonuma <tobi462@gmail.com>
2020-07-25 21:08:27 +0900

    feature: reset to default

--- iOS/SettingView.swift
+++ iOS/SettingView.swift
@@ -10,8 +10,11 @@ import SwiftUI
 private let CellSize: CGFloat = 20
 
 struct SettingView: View {
+    @State private var isAlertPresented = false
     @EnvironmentObject private var setting: SettingEnvironment
-
+    
+    // MARK: Views
+    
     var body: some View {
         Form {
             Section(header: Text("Color")) {
@@ -28,13 +31,47 @@ struct SettingView: View {
                     }
                 }
             }
+            Section {
+                HStack {
+                    Spacer()
+                    Button("Reset to Default", action: tapResetToDefault)
+                        .foregroundColor(.red)
+                        .alert(isPresented: $isAlertPresented, content: resetAlert)
+                    Spacer()
+                }
+            }
         }
+    }
+    
+    private func resetAlert() -> Alert {
+        Alert(
+            title: Text("Do you really want to reset?"),
+            primaryButton: .cancel(),
+            secondaryButton: .destructive(Text("Reset"), action: tapReset))
+    }
+    
+    // MARK: Actions
+    
+    private func tapResetToDefault() {
+        isAlertPresented.toggle()
+    }
+    
+    private func tapReset() {
+        setting.resetToDefault()
     }
 }
 
 struct SettingView_Previews: PreviewProvider {
     static var previews: some View {
-        SettingView()
-            .preferredColorScheme(.light)
+        Group {
+            SettingView()
+                .environmentObject(SettingEnvironment())
+                .previewLayout(.fixed(width: 370.0, height: 250.0))
+                .preferredColorScheme(.light)
+            SettingView()
+                .environmentObject(SettingEnvironment())
+                .previewLayout(.fixed(width: 370.0, height: 250.0))
+                .preferredColorScheme(.dark)
+        }
     }
 }
