65b77151acd75c63090ba4f4f31b45fca49e6bad
Yusuke Hosonuma <tobi462@gmail.com>
2020-07-25 19:00:30 +0900

    fix: build error

--- Shared/Extensions/Color+.swift
+++ Shared/Extensions/Color+.swift
@@ -9,12 +9,21 @@ import SwiftUI
 
 extension Color: RawRepresentable {
     public var rawValue: Data {
+        #if os(macOS)
+        let color = NSColor(self)
+        #else
         let color = UIColor(self)
+        #endif
         return try! NSKeyedArchiver.archivedData(withRootObject: color, requiringSecureCoding: false)
     }
 
     public init?(rawValue: Data) {
-        guard let color = try? NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(rawValue) as? UIColor else { return nil }
+        guard let data = try? NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(rawValue) else { return nil }
+        #if os(macOS)
+        guard let color = data as? NSColor else { return nil }
+        #else
+        guard let color = data as? UIColor else { return nil }
+        #endif
         self.init(color)
     }
 }
--- Shared/View/SettingView.swift
+++ Shared/View/SettingView.swift
@@ -29,7 +29,7 @@ struct SettingView: View {
                         }
                     }
                 }
-            }.navigationBarTitle("Settings")
+            }
         }
     }
 }
