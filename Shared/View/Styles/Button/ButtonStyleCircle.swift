35df0f9c38e6cfa94c709b25cc7535f59bba6e96
Yusuke Hosonuma <tobi462@gmail.com>
2020-07-31 02:32:41 +0900

    fix: style

--- Shared/View/Styles/Button/ButtonStyleCircle.swift
+++ Shared/View/Styles/Button/ButtonStyleCircle.swift
@@ -17,12 +17,17 @@ struct ButtonStyleCircle: ButtonStyle {
         var foreground = Color.white
         var background = Color.accentColor
         
+        var borderColor: Color {
+            colorScheme == .dark ? Color.black : Color.white
+        }
+        
+        @Environment(\.colorScheme) private var colorScheme: ColorScheme
         @Environment(\.isEnabled) private var isEnabled: Bool
         
         var body: some View {
             Circle()
                 .fill(background.opacity(!isEnabled ? 0.5 : configuration.isPressed ? 0.8 : 1))
-                .overlay(Circle().strokeBorder(foreground).padding(3))
+                .overlay(Circle().strokeBorder(borderColor, lineWidth: 2).padding(2))
                 .overlay(configuration.label.foregroundColor(foreground))
                 .frame(width: 75, height: 75)
         }
