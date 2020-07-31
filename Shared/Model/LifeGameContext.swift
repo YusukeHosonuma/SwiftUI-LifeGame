227bd247efb88ef56978a9fc2816eb8697d3fb76
Yusuke Hosonuma <tobi462@gmail.com>
2020-07-31 14:54:48 +0900

    refactor: update LifeGame package to 1.2.0 and use library's api

--- Shared/Model/LifeGameContext.swift
+++ Shared/Model/LifeGameContext.swift
@@ -91,9 +91,7 @@ final class LifeGameContext {
     }
     
     func generateRandom() {
-        let size = _board.value.size
-        let cells = (0 ..< size * size).map { _ in Bool.random() ? 1 : 0 }
-        _board.value = LifeGameBoard(size: size, cells: cells)
+        _board.value = LifeGameBoard.random(size: _board.value.size)
     }
     
     func setPreset(_ preset: BoardPreset) {
