e0d3fbb10643909b7b310570773942f178bc9524
Yusuke Hosonuma <tobi462@gmail.com>
2020-08-07 18:15:42 +0900

    clean: remove unused

--- Shared/View/Screen/MainGame/ControlView.swift
+++ Shared/View/Screen/MainGame/ControlView.swift
@@ -7,89 +7,6 @@
 
 import SwiftUI
 
-// FIXME: mac版の対応が終わったら消す
-struct TopControlView: View {
-    @EnvironmentObject var setting: SettingEnvironment
-    @ObservedObject var viewModel: MainGameViewModel
-
-    // MARK: Private
-    
-    @State private var isPresentedAlert = false
-    @State private var isPresentedSheet = false
-    
-    // MARK: View
-    
-    var body: some View {
-        #if os(macOS)
-        VStack {
-            HStack {
-                Spacer()
-                clearButton()
-            }
-            presetsMenuButton()
-        }
-        .padding()
-        #else
-        HStack {
-            clearButton()
-            presetsMenuButton()
-            Spacer()
-            //Stepper(value: $setting.zoomLevel, in: 0...10) {}
-        }
-        .padding([.leading, .trailing])
-        #endif
-    }
-    
-    private func clearButton() -> some View {
-        Button("Clear") {
-            isPresentedAlert.toggle()
-        }
-        .buttonStyle(ButtonStyleRounded(color: .red))
-        .alert(isPresented: $isPresentedAlert, content: clearAlert)
-    }
-    
-    @ViewBuilder
-    private func presetsMenuButton() -> some View {
-        #if os(macOS)
-        Menu("Presets", content: presetsMenuContent)
-        #else
-        Menu(content: presetsMenuContent) {
-            Button("Presets") {}.buttonStyle(ButtonStyleRounded())
-        }
-        #endif
-    }
-    
-    @ViewBuilder
-    private func presetsMenuContent() -> some View {
-        // Note:
-        // バグか仕様なのか分からないが、実際のボタン表示は逆順になる。（Xcode 12 beta3）
-        //
-        // ...
-        // -----
-        // Random
-        //
-        
-        Button("Random") {
-            viewModel.tapRandomButton()
-        }
-        Divider()
-        ForEach(BoardPreset.allCases, id: \.rawValue) { preset in
-            Button(preset.displayText) {
-                viewModel.selectPreset(preset)
-            }
-        }
-    }
-    
-    // MARK: Actions
-    
-    private func clearAlert() -> Alert {
-        Alert(
-            title: Text("Do you want to clear?"),
-            primaryButton: .cancel(),
-            secondaryButton: .destructive(Text("Clear"), action: viewModel.tapClear))
-    }
-}
-
 struct ControlView: View {
     @StateObject var boardListViewModel = BoardListViewModel()
     @ObservedObject var viewModel: MainGameViewModel
@@ -138,9 +55,9 @@ struct ControlView: View {
     }
 }
 
-struct ControlView_Previews: PreviewProvider {
-    static var previews: some View {
-        TopControlView(viewModel: MainGameViewModel())
-            .previewLayout(.sizeThatFits)
-    }
-}
+//struct ControlView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ControlView(viewModel: MainGameViewModel())
+//            .previewLayout(.sizeThatFits)
+//    }
+//}
