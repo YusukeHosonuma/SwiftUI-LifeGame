f136eb799fe48a542d29f857f8700255033afd07
Yusuke Hosonuma <tobi462@gmail.com>
2020-08-10 01:03:58 +0900

    improve: transition between grid and list

--- iOS/Screen/BoardSelect/BoardGridCell.swift
+++ iOS/Screen/BoardSelect/BoardGridCell.swift
@@ -10,12 +10,36 @@ import LifeGame
 
 struct BoardGridCell: View {
     var item: BoardDocument
+    var style: BoardSelectStyle
     
+    @Namespace private var nspace
+
     var body: some View {
-        VStack {
-            BoardThumnailImage(board: item.makeBoard().extended(by: .die))
-            Text(item.title)
-                .font(.subheadline)
+        switch style {
+        case .grid:
+            VStack {
+                BoardThumnailImage(board: item.makeBoard().extended(by: .die))
+                    .matchedGeometryEffect(id: "thumbnail-\(item.title)", in: nspace)
+                Text(item.title)
+                    .font(.subheadline)
+                    .fixedSize(horizontal: true, vertical: true)
+                    .matchedGeometryEffect(id: "title-\(item.title)", in: nspace)
+            }
+
+        case .list:
+            VStack(spacing: 0) {
+                HStack() {
+                    Text("\(item.title)")
+                        .font(.subheadline)
+                        .matchedGeometryEffect(id: "title-\(item.title)", in: nspace)
+                        .fixedSize(horizontal: true, vertical: true)
+                    Spacer()
+                    BoardThumnailImage(board: item.makeBoard().extended(by: .die))
+                        .frame(width: 60, height: 60, alignment: .center)
+                        .matchedGeometryEffect(id: "thumbnail-\(item.title)", in: nspace)
+                }.padding()
+                Divider()
+            }
         }
     }
 }
@@ -23,20 +47,29 @@ struct BoardGridCell: View {
 struct BoardGridCell_Previews: PreviewProvider {
     
     static var previews: some View {
-        view(title: "Nebura", board: BoardPreset.nebura.board, colorScheme: .light)
-        view(title: "Nebura", board: BoardPreset.nebura.board, colorScheme: .dark)
-        view(title: "SpaceShip", board: BoardPreset.spaceShip.board, colorScheme: .dark)
+        Group {
+            gridView(title: "Nebura",    board: BoardPreset.nebura.board,    colorScheme: .light)
+            gridView(title: "Nebura",    board: BoardPreset.nebura.board,    colorScheme: .dark)
+            gridView(title: "SpaceShip", board: BoardPreset.spaceShip.board, colorScheme: .dark)
+        }
+        Group {
+            listView(title: "Nebura",    board: BoardPreset.nebura.board,    colorScheme: .light)
+            listView(title: "Nebura",    board: BoardPreset.nebura.board,    colorScheme: .dark)
+            listView(title: "SpaceShip", board: BoardPreset.spaceShip.board, colorScheme: .dark)
+        }
     }
     
     @ViewBuilder
-    private static func view(title: String, board: LifeGameBoard, colorScheme: ColorScheme) -> some View {
-        BoardGridCell(
-            item: BoardDocument(id: nil,
-                                title: title,
-                                size: board.size,
-                                cells: board.cells.map(\.rawValue))
-        )
-        .previewLayout(.fixed(width: 200.0, height: 200.0))
-        .preferredColorScheme(colorScheme)
+    private static func gridView(title: String, board: LifeGameBoard, colorScheme: ColorScheme) -> some View {
+        BoardGridCell(item: BoardDocument(title: title, board: board), style: .grid)
+            .previewLayout(.fixed(width: 200.0, height: 200.0))
+            .preferredColorScheme(colorScheme)
+    }
+    
+    @ViewBuilder
+    private static func listView(title: String, board: LifeGameBoard, colorScheme: ColorScheme) -> some View {
+        BoardGridCell(item: BoardDocument(title: title, board: board), style: .list)
+            .previewLayout(.sizeThatFits)
+            .preferredColorScheme(colorScheme)
     }
 }
