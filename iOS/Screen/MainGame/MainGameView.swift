1d20c8aba4862eeb97fb5561579aff2e4120919f
Yusuke Hosonuma <tobi462@gmail.com>
2020-08-09 15:26:16 +0900

    add: register code that comment-outed

--- iOS/Screen/MainGame/MainGameView.swift
+++ iOS/Screen/MainGame/MainGameView.swift
@@ -15,9 +15,24 @@ import FirebaseFirestoreSwift
 struct MainGameView: View {
     @ObservedObject var viewModel: MainGameViewModel
 
+    // For register:
+    // @State var title: String = ""
+    
     var body: some View {
         GeometryReader { geometry in
             VStack {
+                // For register:
+                //
+                // TextField("Title", text: $title)
+                // Button("Save") {
+                //     let trimed = viewModel.board.board.trimed(by: { $0 == .die })
+                //     let document = BoardDocument(title: title, board: LifeGameBoard(board: trimed))
+                //     _ = try! Firestore.firestore()
+                //         .collection("presets")
+                //         .addDocument(from: document)
+                //     title = ""
+                // }
+                
                 HeaderView(generation: viewModel.board.generation, size: viewModel.board.size)
                 .padding([.horizontal])
 
