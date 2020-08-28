0fd1ef9980ccaa9afa8f26cbf86c0efd41789ef2
Yusuke Hosonuma <tobi462@gmail.com>
2020-08-28 19:27:36 +0900

    refactor: alert

--- Shared/View/Alert/FeedbackAlerts.swift
+++ Shared/View/Alert/FeedbackAlerts.swift
@@ -7,38 +7,29 @@
 
 import SwiftUI
 
-enum FeedbackAlertType {
+enum FeedbackAlert: Int, Identifiable {
     case invalid
     case sendConfirm
     case thanks
-}
-
-enum FeedbackAlerts: Identifiable {
-    case invalid
-    case sendConfirm(action: () -> Void)
-    case thanks(action: () -> Void)
-
-    var alert: Alert {
-        switch self {
-        case .invalid:
-            return Alert(title: Text("Please input title and description."))
-            
-        case let .sendConfirm(action):
-            return Alert(title: Text("Send feedback?"),
-                         primaryButton: .default(Text("Send"), action: action),
-                         secondaryButton: .cancel())
-            
-        case let .thanks(action):
-            return Alert(title: Text("Thanks for feedback!"),
-                         dismissButton: .default(Text("OK"), action: action))
-        }
+    
+    var id: Int { rawValue }
+    
+    static func invalidAlert() -> Alert {
+        Alert(title: Text("Please input title and description."))
     }
-
-    var id: Int {
-        switch self {
-        case .invalid:        return 0
-        case .sendConfirm(_): return 1
-        case .thanks(_):      return 2
-        }
+    
+    static func sendConfirmAlert(tapSend: @escaping () -> Void) -> Alert {
+        Alert(
+            title: Text("Send feedback?"),
+            primaryButton: .default(Text("Send"), action: tapSend),
+            secondaryButton: .cancel()
+        )
+    }
+    
+    static func thanksAlert(tapOK: @escaping () -> Void) -> Alert {
+        Alert(
+            title: Text("Thanks for feedback!"),
+            dismissButton: .default(Text("OK"), action: tapOK)
+        )
     }
 }
--- iOS/Screen/Feedback/FeedbackView.swift
+++ iOS/Screen/Feedback/FeedbackView.swift
@@ -7,33 +7,6 @@
 
 import SwiftUI
 
-private enum FeedbackAlert: Int, Identifiable {
-    case invalid
-    case sendConfirm
-    case thanks
-    
-    var id: Int { rawValue }
-    
-    static func invalidAlert() -> Alert {
-        Alert(title: Text("Please input title and description."))
-    }
-    
-    static func sendConfirmAlert(tapSend: @escaping () -> Void) -> Alert {
-        Alert(
-            title: Text("Send feedback?"),
-            primaryButton: .default(Text("Send"), action: tapSend),
-            secondaryButton: .cancel()
-        )
-    }
-    
-    static func thanksAlert(tapOK: @escaping () -> Void) -> Alert {
-        Alert(
-            title: Text("Thanks for feedback!"),
-            dismissButton: .default(Text("OK"), action: tapOK)
-        )
-    }
-}
-
 struct FeedbackView: View {
     @StateObject private var feedbackManager: FeedbackManager
     @Binding private var isPresented: Bool
