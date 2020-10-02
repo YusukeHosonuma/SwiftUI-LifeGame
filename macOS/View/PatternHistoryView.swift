a25a9f7c5f81a38913d3de25165e49c932b014be
Yusuke Hosonuma <tobi462@gmail.com>
2020-10-02 19:22:02 +0900

    refactor: extract PatternHistoryView

---
+++ macOS/View/PatternHistoryView.swift
@@ -0,0 +1,38 @@
+//
+//  PatternHistoryView.swift
+//  LifeGameApp (macOS)
+//
+//  Created by 細沼祐介 on 2020/10/02.
+//
+
+import SwiftUI
+
+struct PatternHistoryView: View {
+    @StateObject private var manager: PatternSelectManager = .init()
+
+    var body: some View {
+        PatternGridListView(
+            style: .horizontal,
+            patternURLs: manager.historyURLs,
+            didTapItem: didTapItem,
+            didToggleStar: didToggleStar
+        )
+
+    }
+    
+    // MARK: Action
+    
+    private func didTapItem(item: PatternItem) {
+        manager.select(item: item)
+    }
+    
+    private func didToggleStar(item: PatternItem) {
+        manager.toggleStar(item: item)
+    }
+}
+
+struct PatternHistoryView_Previews: PreviewProvider {
+    static var previews: some View {
+        PatternHistoryView()
+    }
+}
